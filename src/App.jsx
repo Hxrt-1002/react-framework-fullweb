d52fbe1c044db276764018818725cec00bad2147
Hxrt-1002 <jirapatlimcharoen@gmail.com>
2025-11-10 22:30:56 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <>
-      <div className=''>
 
+      <NavBar/>
+
+      <div className='container '>
+        <p className='relative inline-block after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-full'>
+          hahhahahahaah
+        </p>
       </div>
     </>
   )
